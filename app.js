bb7e8b47593be20936f86d762d1028d3fc5a733d
Edwin Makhanu <edwinmakhanu630@gmail.com>
2022-11-13 10:19:19 +0300

    rock paper scissors game

---
+++ app.js
@@ -0,0 +1,7 @@
+const ComputerChoice = document.getElementById("computer-choice")
+const userChoice = document.getElementById("user-choice")
+const resultDisplay = document.getElementById("result")
+const possibleSelector = document.querySelectorAll('button')
+
+possibleSelector.forEach(possibleSelector => possibleSelector.addEventListener('click', (e) =>))
+
