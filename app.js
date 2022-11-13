e53f45b979e267630fb9df9ffce2eade4a7f9733
Edwin Makhanu <edwinmakhanu630@gmail.com>
2022-11-13 10:31:16 +0300

    javascript event listeners to be added

--- app.js
+++ app.js
@@ -3,5 +3,4 @@ const userChoice = document.getElementById("user-choice")
 const resultDisplay = document.getElementById("result")
 const possibleSelector = document.querySelectorAll('button')
 
-possibleSelector.forEach(possibleSelector => possibleSelector.addEventListener('click', (e) =>))
-
+// javascript code 
